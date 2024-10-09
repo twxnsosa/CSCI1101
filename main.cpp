e324827fcd7c922d38fdd569033ebf946f51e564
migso <migso@LAPTOP-LB33J44M>
2024-10-09 11:45:53 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age;
+
+	cout << "Enter your age: ";
+	cin >> age;
+
+	cout << "You are " << age << " years old.";
+
+	return 0;
+}
